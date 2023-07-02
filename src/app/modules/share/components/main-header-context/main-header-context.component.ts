a712f12bd293962f317e07fa0d9a2171eccf99ed
SachinSilva99 <sachinmanathunga@gmail.com>
2023-07-02 21:39:19 +0530

    init

---
+++ src/app/modules/share/components/main-header-context/main-header-context.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-header-context',
+  templateUrl: './main-header-context.component.html',
+  styleUrls: ['./main-header-context.component.scss']
+})
+export class MainHeaderContextComponent {
+
+}
--- src/app/modules/share/share.module.ts
+++ src/app/modules/share/share.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ShareRoutingModule } from './share-routing.module';
 import { ShareComponent } from './share.component';
+import { MainHeaderContextComponent } from './components/main-header-context/main-header-context.component';
+import { MainHeaderNavbarComponent } from './components/main-header-context/inner-items/main-header-navbar/main-header-navbar.component';
 
 
 @NgModule({
   declarations: [
-    ShareComponent
+    ShareComponent,
+    MainHeaderContextComponent,
+    MainHeaderNavbarComponent
   ],
   imports: [
     CommonModule,
