9b764d2ee02b2b73263dd924796812981807cb03
SachinSilva99 <sachinmanathunga@gmail.com>
2023-07-06 16:48:41 +0530

    added navbar and one section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
 
   {
-    path: 'process',
+    path: '',
     loadChildren: () =>
       import('./modules/process/process.module').then((m) => m.ProcessModule),
   },
@@ -27,7 +27,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  declarations: []
 })
 export class AppRoutingModule {
 }
--- src/app/modules/process/process.module.ts
+++ src/app/modules/process/process.module.ts
@@ -5,12 +5,22 @@ import {ProcessRoutingModule} from './process-routing.module';
 import {ProcessComponent} from './process.component';
 import {MainPageContextComponent} from './components/main-page-context/main-page-context.component';
 import {ShareModule} from "../share/share.module";
+import { NavbarComponent } from './components/main-page-context/inner-items/navbar/navbar.component';
+import { HeroComponent } from './components/main-page-context/inner-items/hero/hero.component';
+import { MainComponent } from './components/main-page-context/inner-items/main/main.component';
+import { ClientSectionComponent } from './components/main-page-context/inner-items/main/client-section/client-section.component';
+import { AboutUsComponent } from './components/main-page-context/inner-items/main/about-us/about-us.component';
 
 
 @NgModule({
   declarations: [
     ProcessComponent,
-    MainPageContextComponent
+    MainPageContextComponent,
+    NavbarComponent,
+    HeroComponent,
+    MainComponent,
+    ClientSectionComponent,
+    AboutUsComponent,
   ],
   imports: [
     CommonModule,
